c712e387bd5f361405dcbe5ccd3e996ec6f02549
Artem Zverev <undemol133@gmail.com>
2022-12-01 02:27:09 +0400

    fix: meta

--- components/main-head/index.tsx
+++ components/main-head/index.tsx
@@ -10,6 +10,8 @@ const MainHead: FC<Props> = ({ title, description }) => (
   <Head>
     <title>{title}</title>
     <meta name="description" content={description} />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="https://ibb.co/qF3xs2z" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
 );
