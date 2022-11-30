8cf01e3d7a5dc1aae645a0683b2af8b2788fc309
Artem Zverev <undemol133@gmail.com>
2022-12-01 02:59:57 +0400

    fix: meta

--- components/main-head/index.tsx
+++ components/main-head/index.tsx
@@ -11,12 +11,14 @@ const MainHead: FC<Props> = ({ title, description }) => (
     <title>{title}</title>
     <meta name="description" content={description} />
     <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://multicomponents.vercel.app/" />
     <meta property="og:image" content="https://ibb.co/qF3xs2z" />
     <meta property="og:image:secure_url" content="https://ibb.co/qF3xs2z" />
     <meta property="og:image:width" content="1920" />
     <meta property="og:image:height" content="1080" />
     <meta property="og:image:alt" content="Screenshot" />
     <meta property="og:title" content={title} />
+    <meta property="og:site_name" content={title} />
     <link rel="icon" href="/favicon.ico" />
   </Head>
 );
